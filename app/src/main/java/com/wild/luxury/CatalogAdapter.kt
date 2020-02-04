652906dd33d9ecd0fbfd6219d53907281b0c29f7
DinozaurG <49711433+DinozaurG@users.noreply.github.com>
2020-02-04 12:15:37 +0700

    Added simple RecyclerView for catalog

---
+++ app/src/main/java/com/wild/luxury/CatalogAdapter.kt
@@ -0,0 +1,28 @@
+package com.wild.luxury
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CatalogAdapter(val items: ArrayList<String>): RecyclerView.Adapter<CatalogAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view : View = LayoutInflater.from(parent.context).inflate(R.layout.catalog_row, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.nme.text = items[position]
+
+    }
+    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val img: TextView = itemView.findViewById(R.id.imageOfTovar)
+        val price: TextView = itemView.findViewById(R.id.priceOfTovar)
+        val desc: TextView = itemView.findViewById(R.id.describeOfTovar)
+        val nme: TextView = itemView.findViewById(R.id.nameOfTovar)
+    }
+}
---
+++ app/src/main/java/com/wild/luxury/CatalogCard.kt
@@ -0,0 +1,5 @@
+package com.wild.luxury
+
+class CatalogCard {
+
+}
