b5cf2de6da15e55355af1b13e1822cbbc9d5d588
Artyom Shklyarskiy <tw1nkyy@yandex.ru>
2020-02-04 14:38:29 +0700

    Merge branch 'master' of C:\Users\saves\Documents\GitHub\Luxury with conflicts.

---
+++ app/src/main/java/com/wild/luxury/ProductDescriprion.kt
@@ -0,0 +1,29 @@
+package com.wild.luxury
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.product_description.*
+import kotlin.text.toInt as toInt1
+
+@SuppressLint("Registered")
+class ProductDescriprion : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.product_description)
+
+
+
+
+        var numProduct : TextView = findViewById(R.id.textNum)
+
+        numProduct.text =getText(R.string.productNumBuy)
+
+        addButton.setOnClickListener {
+        }
+    }
+}
+
+
