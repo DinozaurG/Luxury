944ed2239f3bdcd4f7a2f000f09d1278c3f49a54
AlexanderD <shadesave1999@gmail.com>
2020-02-06 16:51:32 +0700

    great update roomType v2

--- app/src/main/java/com/wild/luxury/adapters/RoomAdapter.kt
+++ app/src/main/java/com/wild/luxury/adapters/RoomAdapter.kt
@@ -7,8 +7,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.wild.luxury.Product
 import com.wild.luxury.R
 import kotlinx.android.synthetic.main.activity_main_room_item.view.*
+import kotlinx.android.synthetic.main.activity_main_room_item_image.view.*
 
-class RoomAdapter(): RecyclerView.Adapter<RoomViewHolder>() {
+class RoomAdapter(): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     var list : List<Product> = emptyList()
     set (value) {
@@ -16,21 +17,35 @@ class RoomAdapter(): RecyclerView.Adapter<RoomViewHolder>() {
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RoomViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.activity_main_room_item, parent, false)
-        return RoomViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == 0) {
+            RoomViewHolder(LayoutInflater.from(parent.context)
+                .inflate(R.layout.activity_main_room_item, parent, false))
+        } else {
+            ImageViewHolder(LayoutInflater.from(parent.context)
+                .inflate(R.layout.activity_main_room_item_image, parent, false))
+        }
     }
+
+    override fun getItemViewType(position: Int): Int = if (position != 0) 0 else 1
 
     override fun getItemCount(): Int = list.size
 
-    override fun onBindViewHolder(holder: RoomViewHolder, position: Int) =
-        holder.bind(list[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        return if (holder.itemViewType == 0) {
+            (holder as RoomViewHolder).bind(list[position-1])
+        } else {
+            (holder as ImageViewHolder).bind()
+        }
+    }
 }
-class RoomViewHolder(
-    itemView: View
-): RecyclerView.ViewHolder(itemView) {
+class RoomViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     fun bind(product: Product) {
         itemView.item_name.text = product.name
+    }
+}
+class ImageViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    fun bind() {
+        itemView.roomImage.setImageResource(R.drawable.econom_hotel_room)
     }
 }
