daa3301ca1ae433283611d69e544d640b352d0b0
DinozaurG <49711433+DinozaurG@users.noreply.github.com>
2020-02-06 17:26:37 +0700

    Added toolbar for catalog.

--- app/src/main/java/com/wild/luxury/activities/Catalog.kt
+++ app/src/main/java/com/wild/luxury/activities/Catalog.kt
@@ -3,6 +3,7 @@ package com.wild.luxury.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.wild.luxury.Product
@@ -14,11 +15,13 @@ class Catalog : AppCompatActivity(),
     OnItemClickListener {
 
     val items: ArrayList<Product> = ArrayList()
+    lateinit var toolbar : Toolbar
     private lateinit var adapter: CatalogAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_catalog)
-
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
         for (i in 1..10) {
             items.add(Product("Table $i", 500 * i,3,"ASD"))
         }
