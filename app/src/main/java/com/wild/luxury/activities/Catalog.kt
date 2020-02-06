d4fe1f9aeff5ddc6f87e22b68577ce3568af0d13
DinozaurG <49711433+DinozaurG@users.noreply.github.com>
2020-02-06 18:28:28 +0700

    Added NORMAL Search(fucking)View into Toolbar

--- app/src/main/java/com/wild/luxury/activities/Catalog.kt
+++ app/src/main/java/com/wild/luxury/activities/Catalog.kt
@@ -1,32 +1,40 @@
 package com.wild.luxury.activities
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.wild.luxury.Product
+import com.wild.luxury.R
 import com.wild.luxury.adapters.CatalogAdapter
 import com.wild.luxury.adapters.OnItemClickListener
-import com.wild.luxury.R
 import com.wild.luxury.network.App
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+//import sun.jvm.hotspot.utilities.IntArray
+
 
 class Catalog : AppCompatActivity(),
     OnItemClickListener {
 
-    val items: ArrayList<Product> = ArrayList()
-    lateinit var toolbar : Toolbar
+    //val items: ArrayList<Product> = ArrayList()
+    private lateinit var toolbar : Toolbar
     private lateinit var adapter: CatalogAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_catalog)
         toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
             /*for (i in 1..10) {
             items?.add(Product("Table $i", 500 * i,3,"ASD"))
         }*/
@@ -37,6 +45,17 @@ class Catalog : AppCompatActivity(),
         itemsList.layoutManager = LinearLayoutManager(this)
         adapter = CatalogAdapter(items, this)
         itemsList.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.catalog_menu, menu)
+
+        val searchItem: MenuItem? = menu?.findItem(R.id.action_search)
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView: SearchView? = searchItem?.actionView as SearchView
+
+        searchView?.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        return super.onCreateOptionsMenu(menu)
     }
 
     override fun onItemClicked(item: Product) {
